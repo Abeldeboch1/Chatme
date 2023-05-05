4d76144f8233ba5f4d4f9fa9f14c37056f765b0b
Abeldeboch1 <abeldeboch1@gmail.com>
2023-05-05 00:09:25 -0600

    some text updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
+import OptionSelection from "./components/OptionSelection";
+import Translation from "./components/Translation";
+import { arrayItems } from "./AIOptions";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_Open_AI_Key,
+  });
+  const openai = new OpenAIApi(configuration);
+  const [option, setOption] = useState({});
+  const [result, setResult] = useState("");
+  const [input, setInput] = useState("");
+  // console.log(import.meta.env.VITE_Open_AI_Key);
+  const selectOption = (option) => {
+    setOption(option);
+  };
+
+  const doStuff = async () => {
+    let object = { ...option, prompt: input };
+
+    const response = await openai.createCompletion(object);
+
+    setResult(response.data.choices[0].text);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {Object.values(option).length === 0 ? (
+        <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+      ) : (
+        <Translation doStuff={doStuff} setInput={setInput} result={result} />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Translation.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Translation({ doStuff, setInput, result }) {
+  return (
+    <div>
+      <textarea
+        className="text-area"
+        cols={55}
+        placeholder="type here..."
+        rows={10}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <button className="action-btn" onClick={doStuff}>
+        GENERATE
+      </button>
+
+      <h3 className="result-text">{result.length > 0 ? result : ""}</h3>
+    </div>
+  );
+}
